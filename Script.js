316bb93494505601066903dbf0a19a33790c9669
Abdulazez <abzaek@hotmail.com>
2023-08-22 10:16:52 +0300

    unlinked styles.css and edited with bootstrap, also modified some elements

--- Script.js
+++ Script.js
@@ -4,36 +4,48 @@ document.getElementById('taskForm').addEventListener('submit', function(e) {
     let taskDescription = document.getElementById('Task').value;
     let isDone = document.getElementById('isDone').checked;
     let deadline = document.getElementById('datetime').value;
-
-    let tableBody = document.querySelector('table');
-    let newRow = document.createElement('tr');
-
-    let descriptionCell = document.createElement('td');
-    let deadlineCell = document.createElement('td');
-    let isDoneCell = document.createElement('td');
-    let deleteCell = document.createElement('td');
-
-    descriptionCell.textContent = taskDescription ? taskDescription : "Not set";
-    isDoneCell.textContent = isDone ? 'Yes' : 'No';
-    deadlineCell.textContent = deadline ? deadline : "Not set";
     
-    
-    let deleteButton = document.createElement('button');
-    deleteButton.textContent = 'DELETE';
-    deleteButton.classList.add('buttons');
-
-    deleteButton.addEventListener('click', function() {
-        var row = this.closest('tr');
-        row.remove();
+    // Reset Button deletes the all the table entries
+    document.getElementById('resetBtn').addEventListener('click', function() {
+        let tbody = document.querySelector('#tbody');
+        tbody.innerHTML = '';
     });
-    // let leftContainer = document.querySelector('.left-container');
-    // leftContainer.style.height = '100%';
 
-    newRow.appendChild(descriptionCell);
-    newRow.appendChild(deadlineCell);
-    newRow.appendChild(isDoneCell);
-    deleteCell.appendChild(deleteButton);
-    newRow.appendChild(deleteCell);
 
-    tableBody.appendChild(newRow);
+    function formToTable() {
+        let tableBody = document.querySelector('table');
+        let newRow = document.createElement('tr');
+
+        let descriptionCell = document.createElement('td');
+        let deadlineCell = document.createElement('td');
+        let isDoneCell = document.createElement('td');
+        let deleteCell = document.createElement('td');
+
+        descriptionCell.textContent = taskDescription;
+        isDoneCell.textContent = isDone ? 'Yes' : 'No';
+        deadlineCell.textContent = deadline;
+
+        let deleteButton = document.createElement('button');
+        deleteButton.textContent = 'DELETE';
+        deleteButton.classList.add('btn','btn-sm','btn-danger','buttons');
+
+        deleteButton.addEventListener('click', function() {
+            var row = this.closest('tr');
+            row.remove();
+        });
+
+        newRow.appendChild(descriptionCell);
+        newRow.appendChild(deadlineCell);
+        newRow.appendChild(isDoneCell);
+        deleteCell.appendChild(deleteButton);
+        newRow.appendChild(deleteCell);
+
+        tableBody.appendChild(newRow);
+    }
+    
+    //denies an empty entry
+    !taskDescription ? !deadline ? alert("Your submission is empty") : alert("please enter your task description") : !deadline ? alert("please enter the deadline") : formToTable();
+
+    
 });
+
